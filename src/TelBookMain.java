049a12bf6a8928ba987c271df2d10ed431d56bc5
yjkogz <yjkogz@gmail.com>
2026-04-22 10:02:37 +0900

    수정

---
+++ src/TelBookMain.java
@@ -0,0 +1,49 @@
+import db.DBConn;
+import exception.MyException;
+import repository.TelBookRepository;
+import service.TelBookService;
+import view.UserView;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class TelBookMain {
+    public static void main(String[] args) throws MyException {
+//        Connection connection = DBConn.getConnection();
+
+        Scanner sc = new Scanner(System.in);
+        TelBookRepository repository = new TelBookRepository();
+        TelBookService service = new TelBookService(repository);
+
+        // UserView 인스턴스를 생성
+        UserView userView = new UserView(sc, service);
+
+        int input;
+        while (true) {
+            do {
+                System.out.println("1.입력 2.수정 3.삭제 4.전체출력 5.ID검색 6.종료");
+                System.out.println("▶ 메뉴 입력 : ");
+                input = sc.nextInt();
+            } while (input <1 || input > 6);
+            switch (input) {
+                case 1:
+                    userView.insert();
+                    break;
+                case 2:
+                    userView.update();
+                    break;
+                case 3:
+                    userView.delete();
+                    break;
+                case 4:
+                    userView.searchAll();
+                    break;
+                case 5:
+                    userView.searchOne();
+                    break;
+                case 6:
+                    System.out.println("종료합니다.");
+            }
+        }
+    }
+}
---
+++ src/view/UserView.java
@@ -0,0 +1,87 @@
+package view;
+
+import exception.InputValidation;
+import exception.MyException;
+import service.TelBookService;
+
+import java.util.Scanner;
+
+public class UserView {
+    private final Scanner scanner;
+    private final TelBookService service;
+
+    public UserView(Scanner scanner, TelBookService service) {
+        this.scanner = scanner;
+        this.service = service;
+    }
+
+    public void insert() throws MyException{
+        // 검증 클래스 생성
+        InputValidation validation = new InputValidation();
+        // 입력자료 저장을 위한 변수 선언
+        String name="";
+        int age=0;
+        String phone="";
+        String address="";
+
+        // 이름, 나이, 전화번호, 주소
+        System.out.println("== 전화번호 등록 ==");
+        // 이름 옳바른 값이 들어올 때 까지 반복
+        boolean nameOk = false;
+        do {
+            try {
+                // 이름 : 무조건 한글만.. 중간공백 없이
+                System.out.println("이름 : ");
+                name = scanner.next();
+                validation.nameCheck(name);
+                nameOk = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (! nameOk);
+
+        // 나이 : 0세 ~ 120세 사이값
+        boolean ageOk = false;
+        do {
+            try {
+                System.out.println("나이 : ");
+                age = scanner.nextInt();
+                validation.ageCheck(age);
+                ageOk = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (! ageOk);
+
+        // 주소
+        System.out.println("주소 : ");
+        address = scanner.next();
+
+        // 전화번호(010-XXXX-XXXX)
+        boolean phoneOk = false;
+        do {
+            try {
+                System.out.println("전화번호 : ");
+                phone = scanner.next();
+                validation.phoneCheck(phone);
+                phoneOk = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (! phoneOk);
+
+        service.insert(name, age, address, phone);
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void searchAll() {
+    }
+
+    public void searchOne() {
+    }
+}
